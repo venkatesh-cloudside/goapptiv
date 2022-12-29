d5231f761e692e52274f01d2d8cb2a8a7298ee9e
venkatesh-cloudside <119586525+venkatesh-cloudside@users.noreply.github.com>
2022-12-29 23:42:17 +0530

    Update IMPORTANTS.sql

--- models/example/IMPORTANTS.sql
+++ models/example/IMPORTANTS.sql
@@ -11,7 +11,7 @@ json_extract_scalar(_airbyte_data, "$['headquarter_code']") as headquarter_code,
 json_extract_scalar(_airbyte_data, "$['organization_code']") as organization_code,
 json_extract_scalar(_airbyte_data, "$['department_code']") as department_code,
 json_extract_string_array(_airbyte_data, "$['doctor_ids']") as doctor_ids
-FROM goapptiv-data-lake.airbyte_mongodb_doctor_mangement._airbyte_raw_IMPORTANTS
+FROM goapptiv-data-lake.airbyte_test._airbyte_raw_IMPORTANTS
 
 )
 
