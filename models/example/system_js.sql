fcbb904a97cf57921448a094928c9e39c250c848
venkatesh-cloudside <119586525+venkatesh-cloudside@users.noreply.github.com>
2022-12-29 23:52:50 +0530

    Update system_js.sql

--- models/example/system_js.sql
+++ models/example/system_js.sql
@@ -7,7 +7,7 @@ system_js AS (
 SELECT
 json_extract_scalar(_airbyte_data, "$['_id']") as _id,
 json_extract_scalar(_airbyte_data, "$['value']") as value,
-FROM goapptiv-data-lake.airbyte_mongodb_doctor_mangement._airbyte_raw_system_js
+FROM goapptiv-data-lake.airbyte_test.system_js
 
 )
 
