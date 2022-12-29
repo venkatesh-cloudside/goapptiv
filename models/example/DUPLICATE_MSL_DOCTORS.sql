dd524ab524f1316837fb6c918228e3f9311e75d4
venkatesh-cloudside <119586525+venkatesh-cloudside@users.noreply.github.com>
2022-12-29 23:38:34 +0530

    Update DUPLICATE_MSL_DOCTORS.sql

--- models/example/DUPLICATE_MSL_DOCTORS.sql
+++ models/example/DUPLICATE_MSL_DOCTORS.sql
@@ -9,7 +9,7 @@ json_extract(_airbyte_data, "$['_id']") as _id,
 json_extract_scalar(_airbyte_data, "$['count']") as count,
 json_extract_scalar(_airbyte_data, "$['new_created_at']") as new_created_at,
 json_extract_string_array(_airbyte_data, "$['object_ids']") as object_ids
-FROM goapptiv-data-lake.airbyte_mongodb_doctor_mangement._airbyte_raw_DUPLICATE_MSL_DOCTORS
+FROM goapptiv-data-lake.airbyte_test._airbyte_raw_DUPLICATE_MSL_DOCTORS
 
 )
 
