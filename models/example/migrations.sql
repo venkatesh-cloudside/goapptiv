96712f7ac6c451229f47d277c09aa14f41b06160
venkatesh-cloudside <119586525+venkatesh-cloudside@users.noreply.github.com>
2022-12-29 23:52:26 +0530

    Update migrations.sql

--- models/example/migrations.sql
+++ models/example/migrations.sql
@@ -8,7 +8,7 @@ SELECT
 json_extract_scalar(_airbyte_data, "$['_id']") as _id,
 json_extract_scalar(_airbyte_data, "$['migration']") as migration,
 json_extract_scalar(_airbyte_data, "$['batch']") as batch,
-FROM goapptiv-data-lake.airbyte_mongodb_doctor_mangement._airbyte_raw_migrations
+FROM goapptiv-data-lake.airbyte_test._airbyte_raw_migrations
 
 )
 
