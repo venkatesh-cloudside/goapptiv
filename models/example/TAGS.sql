fdef167c678f4fb0e8f88014d46050ef90212345
venkatesh-cloudside <119586525+venkatesh-cloudside@users.noreply.github.com>
2022-12-29 23:51:59 +0530

    Update TAGS.sql

--- models/example/TAGS.sql
+++ models/example/TAGS.sql
@@ -8,7 +8,7 @@ SELECT
 json_extract_scalar(_airbyte_data, "$['_id']") as _id,
 json_extract_scalar(_airbyte_data, "$['name']") as name,
 json_extract_scalar(_airbyte_data, "$['cards']") as cards,
-FROM goapptiv-data-lake.airbyte_mongodb_doctor_mangement._airbyte_raw_TAGS
+FROM goapptiv-data-lake.airbyte_test._airbyte_raw_TAGS
 
 )
 
