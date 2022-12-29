8aa4638fa11e5f7d910b3441c4650813ead37b84
venkatesh-cloudside <119586525+venkatesh-cloudside@users.noreply.github.com>
2022-12-29 23:51:03 +0530

    Update RESPI_DOCTORS.sql

--- models/example/RESPI_DOCTORS.sql
+++ models/example/RESPI_DOCTORS.sql
@@ -9,7 +9,7 @@ json_extract_scalar(_airbyte_data, "$['_id']") as _id,
 json_extract_scalar(_airbyte_data, "$['full_name']") as full_name,
 json_extract_scalar(_airbyte_data, "$['msl_code']") as msl_code,
 json_extract_scalar(_airbyte_data, "$['headquarter_code']") as headquarter_code
-FROM goapptiv-data-lake.airbyte_mongodb_doctor_mangement._airbyte_raw_RESPI_DOCTORS
+FROM goapptiv-data-lake.airbyte_test._airbyte_raw_RESPI_DOCTORS
 
 )
 
