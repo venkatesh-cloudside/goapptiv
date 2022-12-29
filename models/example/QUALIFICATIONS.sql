6957537ed304a95315a36fdc61be967e9b853c48
venkatesh-cloudside <119586525+venkatesh-cloudside@users.noreply.github.com>
2022-12-29 23:45:38 +0530

    Update QUALIFICATIONS.sql

--- models/example/QUALIFICATIONS.sql
+++ models/example/QUALIFICATIONS.sql
@@ -9,7 +9,7 @@ json_extract_scalar(_airbyte_data, "$['_id']") as _id,
 json_extract_scalar(_airbyte_data, "$['name_aibyte_transform']") as name,
 json_extract_scalar(_airbyte_data, "$['updated_at']") as updated_at,
 json_extract_scalar(_airbyte_data, "$['created_at']") as created_at
-FROM goapptiv-data-lake.airbyte_mongodb_doctor_mangement._airbyte_raw_QUALIFICATIONS
+FROM goapptiv-data-lake.airbyte_test._airbyte_raw_QUALIFICATIONS
 
 )
 SELECT * FROM QUALIFICATIONS
