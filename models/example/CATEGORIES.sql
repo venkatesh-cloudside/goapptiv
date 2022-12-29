4b88e5d98e17fcc004857f901114dcc054df1e58
venkatesh-cloudside <119586525+venkatesh-cloudside@users.noreply.github.com>
2022-12-29 23:36:47 +0530

    Update CATEGORIES.sql

--- models/example/CATEGORIES.sql
+++ models/example/CATEGORIES.sql
@@ -7,7 +7,7 @@ CATEGORIES AS (
 SELECT
 json_extract_scalar(_airbyte_data, "$['_id']") as _id,
 json_extract_scalar(_airbyte_data, "$['name']") as name
-FROM goapptiv-data-lake.airbyte_mongodb_doctor_mangement._airbyte_raw_CATEGORIES
+FROM goapptiv-data-lake.airbyte_test._airbyte_raw_CATEGORIES
 
 )
 
